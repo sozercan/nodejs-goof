d41cf971833de410d51fdc1af5717a46ff8406e4
Orka Publisher <publisher@orka.ai>
2026-08-30 16:57:49 +0000

    orka: apply agent task nodejs-goof-patch-fnd-3e194f131d22-1788109069

--- routes/index.js
+++ routes/index.js
@@ -48,16 +48,21 @@ exports.index = function (req, res, next) {
 };
 
 exports.loginHandler = function (req, res, next) {
-  if (validator.isEmail(req.body.username)) {
-    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
-      if (users.length > 0) {
-        const redirectPage = req.body.redirectPage
-        const session = req.session
-        const username = req.body.username
-        return adminLoginSuccess(redirectPage, session, username, res)
-      } else {
+  if (typeof req.body.username === 'string' && typeof req.body.password === 'string' && validator.isEmail(req.body.username)) {
+    User.findOne({ username: req.body.username }, function (err, user) {
+      if (err) return next(err);
+      if (!user) return res.status(401).send();
+
+      user.verifyPassword(req.body.password, function (err, passwordMatches) {
+        if (err) return next(err);
+        if (passwordMatches) {
+          const redirectPage = req.body.redirectPage
+          const session = req.session
+          const username = req.body.username
+          return adminLoginSuccess(redirectPage, session, username, res)
+        }
         return res.status(401).send()
-      }
+      });
     });
   } else {
     return res.status(401).send()
