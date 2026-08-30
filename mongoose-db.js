83385c2b1aa1ccb2dba22b303335785ef53065d3
Orka Publisher <publisher@orka.ai>
2026-08-30 16:14:36 +0000

    orka: apply agent task nodejs-goof-patch-fnd-3e194f131d22-1788106476

--- mongoose-db.js
+++ mongoose-db.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 var cfenv = require("cfenv");
+var crypto = require('crypto');
 var Schema = mongoose.Schema;
+
+var PASSWORD_ITERATIONS = 600000;
 
 var Todo = new Schema({
   content: Buffer,
@@ -13,6 +16,36 @@ var User = new Schema({
   username: String,
   password: String,
 });
+
+User.pre('save', function (next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+
+  crypto.randomBytes(16, function (err, salt) {
+    if (err) return next(err);
+    crypto.pbkdf2(user.password, salt, PASSWORD_ITERATIONS, 32, 'sha256', function (err, hash) {
+      if (err) return next(err);
+      user.password = ['pbkdf2', PASSWORD_ITERATIONS, salt.toString('hex'), hash.toString('hex')].join('$');
+      next();
+    });
+  });
+});
+
+User.methods.verifyPassword = function (password, callback) {
+  if (typeof this.password !== 'string') return callback(null, false);
+  var parts = this.password.split('$');
+  if (typeof password !== 'string' || parts.length !== 4 || parts[0] !== 'pbkdf2' ||
+      Number(parts[1]) !== PASSWORD_ITERATIONS) return callback(null, false);
+
+  var salt = Buffer.from(parts[2], 'hex');
+  var expected = Buffer.from(parts[3], 'hex');
+  if (salt.length !== 16 || expected.length !== 32) return callback(null, false);
+
+  crypto.pbkdf2(password, salt, PASSWORD_ITERATIONS, expected.length, 'sha256', function (err, actual) {
+    if (err) return callback(err);
+    callback(null, crypto.timingSafeEqual(expected, actual));
+  });
+};
 
 mongoose.model('User', User);
 
@@ -45,14 +78,24 @@ console.log("Using Mongo URI " + mongoUri);
 mongoose.connect(mongoUri);
 
 User = mongoose.model('User');
-User.find({ username: 'admin@snyk.io' }).exec(function (err, users) {
-  console.log(users);
-  if (users.length === 0) {
-    console.log('no admin');
-    new User({ username: 'admin@snyk.io', password: 'SuperSecretPassword' }).save(function (err, user, count) {
-      if (err) {
-        console.log('error saving admin user');
-      }
+User.findOne({ username: 'admin@snyk.io' }).exec(function (err, user) {
+  if (err) return console.log('error finding admin user');
+
+  function provisionAdmin() {
+    if (!process.env.ADMIN_PASSWORD) {
+      return console.log('ADMIN_PASSWORD is not set; admin user was not provisioned');
+    }
+    new User({ username: 'admin@snyk.io', password: process.env.ADMIN_PASSWORD }).save(function (err) {
+      if (err) console.log('error saving admin user');
     });
   }
+
+  if (user && (typeof user.password !== 'string' || user.password.indexOf('pbkdf2$') !== 0)) {
+    return user.remove(function (err) {
+      if (err) return console.log('error invalidating legacy admin user');
+      provisionAdmin();
+    });
+  }
+
+  if (!user) provisionAdmin();
 });
--- routes/index.js
+++ routes/index.js
@@ -49,15 +49,21 @@ exports.index = function (req, res, next) {
 
 exports.loginHandler = function (req, res, next) {
   if (validator.isEmail(req.body.username)) {
-    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
-      if (users.length > 0) {
-        const redirectPage = req.body.redirectPage
-        const session = req.session
-        const username = req.body.username
-        return adminLoginSuccess(redirectPage, session, username, res)
-      } else {
-        return res.status(401).send()
-      }
+    User.findOne({ username: req.body.username }, function (err, user) {
+      if (err) return next(err);
+      if (!user) return res.status(401).send();
+
+      user.verifyPassword(req.body.password, function (err, valid) {
+        if (err) return next(err);
+        if (valid) {
+          const redirectPage = req.body.redirectPage
+          const session = req.session
+          const username = req.body.username
+          return adminLoginSuccess(redirectPage, session, username, res)
+        } else {
+          return res.status(401).send()
+        }
+      });
     });
   } else {
     return res.status(401).send()
