92ee0d3367262330eea537aad0a4bce30f6407d0
Orka Publisher <publisher@orka.ai>
2026-09-16 18:54:19 +0000

    orka: apply agent task nodejs-goof-patch-fnd-d7affd1d6d26-fe5fc3cc9427

--- mongoose-db.js
+++ mongoose-db.js
@@ -45,14 +45,19 @@ console.log("Using Mongo URI " + mongoUri);
 mongoose.connect(mongoUri);
 
 User = mongoose.model('User');
-User.find({ username: 'admin@snyk.io' }).exec(function (err, users) {
-  console.log(users);
-  if (users.length === 0) {
-    console.log('no admin');
-    new User({ username: 'admin@snyk.io', password: 'SuperSecretPassword' }).save(function (err, user, count) {
-      if (err) {
-        console.log('error saving admin user');
-      }
-    });
-  }
-});
+var adminUsername = process.env.ADMIN_USERNAME;
+var adminPassword = process.env.ADMIN_PASSWORD;
+
+if (adminUsername && adminPassword) {
+  User.find({ username: adminUsername }).exec(function (err, users) {
+    console.log(users);
+    if (users.length === 0) {
+      console.log('no admin');
+      new User({ username: adminUsername, password: adminPassword }).save(function (err, user, count) {
+        if (err) {
+          console.log('error saving admin user');
+        }
+      });
+    }
+  });
+}
